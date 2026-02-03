3e8a5fa260d52aa3ba9fbd5317cd1179befdb79d
neelam177 <gneelam2004@gmail.com>
2026-02-03 11:27:09 +0530

    commit

--- eComm/app/Http/Controllers/EmployeeController.php
+++ eComm/app/Http/Controllers/EmployeeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Employees;
 class EmployeeController extends Controller
 {
     //
-    
+    function list(){
+        return Employees::all();
+    }
 
 }
--- eComm/routes/api.php
+++ eComm/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,5 @@ Route::get('/user', function (Request $request) {
 Route::get("test", function () {
     return ["name" => "Neelam Gupta", 'channel' => "coding steps"];
 });
+
+Route::get('students',[EmployeeController::class,'list']);
